947ef674c0519e3b535bb652ca1e0cf003400b2b
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2026-04-03 13:44:43 -0400

    style: adiciona icone de spinner animado para o estado de loading

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,18 +4,14 @@ import { MovieContext } from "../context/MovieContext";
 import MovieCard from "./MovieCard";
 
 function Search() {
-  // Pega a palavra digitada na URL (ex: /search/batman -> query = "batman")
   const { query } = useParams(); 
   
-  // Trazemos as funções do nosso estado global
   const { movies, searchMovies, loading } = useContext(MovieContext);
 
-  // Assim que o componente carregar (ou a query mudar), ele faz a busca
   useEffect(() => {
     if (query) {
       searchMovies(query);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
   return (
@@ -29,10 +25,8 @@ function Search() {
       ) : (
         <ul className="movies-grid">
           {movies && movies.length > 0 ? (
-            // Se achou filmes, mapeia e cria um MovieCard para cada um
             movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
           ) : (
-            // Se a API retornar vazio
             <p className="no-results">Nenhum filme encontrado para "{query}".</p>
           )}
         </ul>
